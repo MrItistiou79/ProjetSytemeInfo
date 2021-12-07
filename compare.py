8cb4dbb97c6ef7e9c0a6a99ce111b26ffce7fe62
dsfaqfq <hugo.vandermosten1@gmail.com>
2021-12-07 20:34:34 +0100

    encore modif encore modif

--- compare.py
+++ compare.py
@@ -4,63 +4,84 @@ import argparse
 
 def parse():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--input-file-1", help="path to the csv file with the computed results with POSIX", type=str)
-    parser.add_argument("-g", "--input-file-2", help="path to the csv file with the computed results with active waiting", type=str)
+    parser.add_argument("-a", "--check", help="check if 2 of 3 files", type=int)
+    parser.add_argument("-f", "--input-file-1", help="path to the csv file with the computed results with POSIX",
+                        type=str)
+    parser.add_argument("-g", "--input-file-2", help="path to the csv file with the computed results with TAS",
+                        type=str)
+    parser.add_argument("-h", "--input-file-3", help="path to the csv file with the computed results with TATAS",
+                        type=str)
     parser.add_argument("-n", "--title", help="Title of the graph", type=str, default=None)
     return parser.parse_args()
 
 
-def plot(filename_POSIX, filename_active, title):
+def plot3(filename_POSIX, filename_TAS, filename_TATAS, title):
     with open(filename_POSIX, 'r') as f:
-        with open(filename_active, 'r') as g:
+        with open(filename_TAS, 'r') as g:
+            with open(filename_TATAS, 'r') as h:
+                lines_f = f.readlines()[1::]
+                lines_g = g.readlines()[1::]
+                lines_h = h.readline()[1::]
+
+                nbr_threads = list(range(1, 9))  # number of threads
+
+                av_time_1 = []  # average time for POSIX locks and semaphores
+                av_time_2 = []  # average time for TAS locks and semaphores
+                av_time_3 = []  # average time for TATAS locks and semaphores
+                for i in range(0, len(lines_f), 5):
+                    t1 = 0
+                    t2 = 0
+                    t3 = 0
+                    for j in range(5):  # incrementing average times
+                        t1 += float(lines_f[i + j].strip().split(',')[1])
+                        t2 += float(lines_g[i + j].strip().split(',')[1])
+                        t3 += float(lines_h[i + j].strip().split(',')[1])
+                    av_time_1.append(t1 / 5)
+                    av_time_2.append(t2 / 5)
+                    av_time_3.append(t3 / 5)
+
+                # Plotting
+                plt.plot(nbr_threads, av_time_1, label="POSIX")
+                plt.plot(nbr_threads, av_time_2, label="TAS")
+                plt.plot(nbr_threads, av_time_3, label="TATAS")
+                plt.xticks(nbr_threads, nbr_threads)
+                plt.grid(True)
+                plt.title(title)
+
+                plt.savefig("comparaison.png")
+
+
+def plot2(filename_TAS, filename_TATAS, title):
+    with open(filename_TAS, 'r') as f:
+        with open(filename_TATAS, 'r') as g:
             lines_f = f.readlines()[1::]
             lines_g = g.readlines()[1::]
-            data_moustache1 = []
-            data_moustache2 = []
-            nbr_threads = list(range(1, 9))
-            av_time_1 = []
-            av_time_2 = []
+
+            nbr_threads = list(range(1, 9))  # number of threads
+
+            av_time_1 = []  # average time for TAS locks testing
+            av_time_2 = []  # average time for TATAS locks testing
             for i in range(0, len(lines_f), 5):
-                # moustache
-                time1 = []
-                time2 = []
                 t1 = 0
                 t2 = 0
-                for j in range(5):
-                    l1 = float(lines_f[i + j].strip().split(',')[1])
-                    l2 = float(lines_g[i + j].strip().split(',')[1])
-                    time1.append(l1)
-                    time2.append(l2)
-                    t1 += l1
-                    t2 += l2
+                for j in range(5):  # incrementing average times
+                    t1 += float(lines_f[i + j].strip().split(',')[1])
+                    t2 += float(lines_g[i + j].strip().split(',')[1])
                 av_time_1.append(t1 / 5)
                 av_time_2.append(t2 / 5)
-                data_moustache1.append(time1)
-                data_moustache2.append(time2)
 
-            fig, ax = plt.subplots(3, 1, figsize=(8, 10))
-
-            ax[0].boxplot(data_moustache1)
-            ax[0].set_xticklabels(nbr_threads)
-            ax[0].set_ylabel("temps [s]", fontsize=12)
-            ax[0].set_title("POSIX", fontsize=15)
-
-            ax[1].boxplot(data_moustache2)
-            ax[1].set_xticklabels(nbr_threads)
-            ax[1].set_ylabel("temps [s]", fontsize=12)
-            ax[1].set_title("Attente active", fontsize=15)
-
-            ax[2].plot(nbr_threads, av_time_1, label = "POSIX")
-            ax[2].plot(nbr_threads, av_time_2, label = "A-A")
-            ax[2].legend()
-            ax[2].set_ylabel("temps [s]", fontsize=12)
-            ax[2].set_xlabel("Nombre de threads", fontsize=12)
+            # Plotting
+            plt.plot(nbr_threads, av_time_1, label="TAS")
+            plt.plot(nbr_threads, av_time_2, label="TATAS")
             plt.xticks(nbr_threads, nbr_threads)
-            ax[2].set_title(title, fontsize=15)
-            plt.subplots_adjust(hspace=0.3)
-            plt.savefig("plot_comparaison.png")
+            plt.grid(True)
+            plt.title(title)
+
+            plt.savefig("comparaison.png")
+
 
 args = parse()
-plot(args.input_file_1, args.input_file_2, args.title)
-
-
+if args.check == 2:
+    plot2(args.args.input_file_1, args.input_file_2, args.title)
+else:
+    plot3(args.input_file_1, args.input_file_2, args.input_file_3, args.title)
